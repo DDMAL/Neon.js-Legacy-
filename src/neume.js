a850b7335869ea24d9b61cd931b8caa74e37a728
Gregory Burlet <gregory.burlet@mail.mcgill.ca>
2012-01-09 12:47:34 -0500

    Neume encoding
    Defined neumes (name and melodic movement) from the Fundamental Neumes Board in the Finale Medieval Plugin
    
    Refs #13

--- src/neume.js
+++ src/neume.js
@@ -53,6 +53,182 @@ Toe.Neume = function(rendEng, options) {
 
 Toe.Neume.prototype.constructor = Toe.Neume;
 
+// Neumes encoded from the Fundamental Neumes Board in Medieval Finale
+Toe.Neume.Type = {
+    punctum: {
+        name: "Punctum",
+        melodicMove: [0],
+    },
+    virga: {
+        name: "Virga",
+        melodicMove: [0]
+    },
+    bivirga: {
+        name: "Bivirga",
+        melodicMove: [0, 0]
+    },
+    distropha: {
+        name: "Distropha",
+        melodicMove: [0, 0]
+    },
+    tristropha: {
+        name: "Tristropha",
+        melodicMove: [0, 0, 0]
+    },
+    clivis: {
+        name: "Clivis",
+        melodicMove: [0, -1]
+    },
+    climacus: {
+        name: "Climacus",
+        melodicMove: [0, -1, -1]
+    },
+    climacus2: {
+        name: "Climacus 2",
+        melodicMove: [0, -1, -1, -1]
+    },
+    climacus3: {
+        name: "Climacus 3",
+        melodicMove: [0, -1, -1, -1, -1]
+    },
+    climacus4: {
+        name: "Climacus 4",
+        melodicMove: [0, -1, -1, -1, -1, -1]
+    },
+    climacusresup: {
+        name: "Climacus resupinus",
+        melodicMove: [0, -1, -1, 1]
+    },
+    climacusresup2: {
+        name: "Climacus resupinus 2",
+        melodicMove: [0, -1, -1, -1, 1]
+    },
+    climacusresup3: {
+        name: "Climacus resupinus 3",
+        melodicMove: [0, -1, -1, -1, -1, 1]
+    },
+    climacusresup4: {
+        name: "Climacus resupinus 4",
+        melodicMove: [0, -1, -1, -1, -1, -1, 1]
+    },
+    podatus: {
+        name: "Podatus (Pes)",
+        melodicMove: [0, 1]
+    },
+    podatussub: {
+        name: "Podatus subpunctis",
+        melodicMove: [0, 1, -1, -1]
+    },
+    podatussub2: {
+        name: "Podatus subpunctis 2",
+        melodicMove: [0, 1, -1, -1, -1]
+    },
+    podatussub3: {
+        name: "Podatus subpunctis 3",
+        melodicMove: [0, 1, -1, -1, -1, -1]
+    },
+    podatussub4: {
+        name: "Podatus subpunctis 4",
+        melodicMove: [0, 1, -1, -1, -1, -1, -1]
+    },
+    podatussubresup: {
+        name: "Podatus Subpunctis resupinus",
+        melodicMove: [0, 1, -1, -1, 1]
+    },
+    podatussubresup2: {
+        name: "Podatus Subpunctis resupinus 2",
+        melodicMove: [0, 1, -1, -1, -1, 1]
+    },
+    podatussubresup3: {
+        name: "Podatus Subpunctis resupinus 3",
+        melodicMove: [0, 1, -1, -1, -1, -1, 1]
+    },
+    scandicus: {
+        name: "Scandicus",
+        melodicMove: [0, 1, 1]
+    },
+    scandicus2: {
+        name: "Scandicus 2",
+        melodicMove: [0, 1, 1, 1]
+    },
+    scandicus3: {
+        name: "Scandicus 3",
+        melodicMove: [0, 1, 1, 1, 1]
+    },
+    scandicusflex: {
+        name: "Scandicus flexus",
+        melodicMove: [0, 1, 1, -1]
+    },
+    scandicusflex2: {
+        name: "Scandicus flexus 2",
+        melodicMove: [0, 1, 1, 1, -1]
+    },
+    scandicusflex3: {
+        name: "Scandicus flexus 3",
+        melodicMove: [0, 1, 1, 1, 1, -1]
+    },
+    scandicussub: {
+        name: "Scandicus subpunctis",
+        melodicMove: [0, 1, 1, -1, -1]
+    },
+    scandicussub2: {
+        name: "Scandicus subpunctis 2",
+        melodicMove: [0, 1, 1, -1, -1, -1]
+    },
+    porrectus: {
+        name: "Porrectus",
+        melodicMove: [0, -1, 1]
+    },
+    porrectusflex: {
+        name: "Porrectus flexus",
+        melodicMove: [0, -1, 1, -1]
+    },
+    porrectussub: {
+        name: "Porrectus subpunctis",
+        melodicMove: [0, -1, 1, -1, -1]
+    },
+    porrectussub2: {
+        name: "Porrectus subpunctis 2",
+        melodicMove: [0, -1, 1, -1, -1, -1]
+    },
+    porrectussubresup: {
+        name: "Porrectus Subpunctis resupinus",
+        melodicMove: [0, -1, 1, -1, -1, 1]
+    },
+    porrecussubresup2: {
+        name: "Porrectus Subpunctis resupinus 2",
+        melodicMove: [0, -1, 1, -1, -1, -1, 1]
+    },
+    compound1: {
+        name: "Compound Neume 1",
+        melodicMove: [0, -1, 1, 1, -1]
+    },
+    compound2: {
+        name: "Compound Neume 2",
+        melodicMove: [0, -1, 1, 1]
+    },
+    torculus: {
+        name: "Torculus",
+        melodicMove: [0, 1, -1]
+    },
+    torculusresup: {
+        name: "Torculus resupinus",
+        melodicMove: [0, 1, -1, 1]
+    },
+    torculusresup2: {
+        name: "Torculus resupinus 2",
+        melodicMove: [0, 1, -1, 1, -1]
+    },
+    torculusresup3: {
+        name: "Torculus resupinus 3",
+        melodicMove: [0, 1, -1, 1, -1, -1]
+    },
+    torculusresup4: {
+        name: "Torculus resupinus 4",
+        melodicMove: [0, 1, -1, 1, -1, -1, -1]
+    }
+}
+
 Toe.Neume.prototype.setBoundingBox = function(bb) {
     // set position
     this.zone.ulx = bb[0];
@@ -86,7 +262,7 @@ Toe.Neume.prototype.neumeFromMei = function(neumeData, facs) {
     $(neumeData).find("note").each(function(it, el) {
         var pname = $(el).attr("pname");
         var oct = parseInt($(el).attr("oct"));
-        //console.log("nc: " + pname + ", " + oct);
+        //DEBUG: console.log("nc: " + pname + ", " + oct);
 
         if (theNeume.components.length == 0) {
             theNeume.props.rootNote.rootPitch = pname;
