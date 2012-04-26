7e04d2f8c10add46f841f9355869aacec8921a3f
Gregory Burlet <gregory.burlet@mail.mcgill.ca>
2012-04-26 13:39:17 -0400

    Added Custos
    model, view, and controller

---
+++ src/custos.js
@@ -0,0 +1,99 @@
+/*
+Copyright (C) 2011 by Gregory Burlet, Alastair Porter
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+
+/**
+ * Creates a new custos 
+ *
+ * @class Models a custos, a neume at the end of a staff that represents the first
+ * note in the next staff.
+ * @param {String} pname pitch name of the next note
+ * @param {Integer} oct octave of the next note
+ */
+Toe.Model.Custos = function(pname, oct, options) {
+    this.props = {
+        interact: true
+    };
+
+    $.extend(this.props, options);
+
+    this.pname = pname;
+    this.oct = oct;
+
+    // the integer pitch difference relative to the clef
+    // this is set when the custos is mounted on a staff
+    this.pitchDiff = null;
+
+    // initialize bounding box
+    this.zone = new Object();
+
+    // set by server or MEI so null by default
+    this.id = null;
+}
+
+Toe.Model.Custos.prototype.constructor = Toe.Model.Custos;
+
+/**
+ * Sets the bounding box of the custos 
+ *
+ * @methodOf Toe.Model.Custos
+ * @param {Array} bb [ulx, uly, lrx, lry]
+ */
+Toe.Model.Custos.prototype.setBoundingBox = function(bb) {
+    if(!Toe.validBoundingBox(bb)) {
+        throw new Error("Division: invalid bounding box");
+    }
+
+    this.zone.ulx = bb[0];
+    this.zone.uly = bb[1];
+    this.zone.lrx = bb[2];
+    this.zone.lry = bb[3];
+}
+
+/**
+ * Sets the pitch name and octave of the custos
+ *
+ * @methodOf Toe.Model.Custos
+ * @param {String} pname pitch name
+ * @param {Integer} oct octave
+ */
+Toe.Model.Custos.prototype.setRootNote = function(pname, oct) {
+    this.pname = pname;
+    this.oct = oct;
+}
+
+/**
+ * Sets the pitch difference of the custos in relation to the clef of the staff the 
+ * custos is mounted on
+ */
+Toe.Model.Custos.prototype.setPitchDiff = function(pitchDiff) {
+    this.pitchDiff = pitchDiff;
+}
+
+/**
+ * Sets the id of the custos element.
+ * 
+ * @methodOf Toe.Model.Custos
+ * @param {String} id
+ */
+Toe.Model.Custos.prototype.setID = function(cid) {
+    this.id = cid;
+}
---
+++ src/custosview.js
@@ -0,0 +1,55 @@
+/*
+Copyright (C) 2011 by Gregory Burlet, Alastair Porter
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+
+/**
+ * Creates a new custos view
+ *
+ * @class View for the custos
+ * @param {Toe.View.RenderEngine} renderEngine The rendering engine
+ */
+Toe.View.CustosView = function(renderEngine) {
+    this.rendEng = renderEngine;
+}
+
+Toe.View.CustosView.prototype.constructor = Toe.View.CustosView;
+
+/**
+ * Renders the custos on the staffs
+ *
+ * @methodOf Toe.View.CustosView
+ * @param {Toe.Model.Custos} custos Custos to render
+ * @param {Toe.Model.Staff} staff Staff context in which to render the provided custos
+ */
+Toe.View.CustosView.prototype.renderCustos = function(cModel, sModel, custos_y) {
+    if (!this.rendEng) {
+        throw new Error("Custos: Invalid render context");
+    }
+
+    var elements = {static: new Array(), modify: new Array()};
+    
+    var glyphCustos = this.rendEng.getGlyph("custos");
+    var custos = glyphCustos.clone().set({left: cModel.zone.ulx + glyphCustos.centre[0], top: custos_y - glyphCustos.centre[1]/2});
+
+    elements.modify.push(custos);
+
+	this.rendEng.draw(elements, {selectable: cModel.props.interact});
+}
