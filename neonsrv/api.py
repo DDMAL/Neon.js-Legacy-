d576931a61187b060941d7e8b10928989d3b2266
Gregory Burlet <gregory.burlet@mail.mcgill.ca>
2012-05-02 18:02:44 -0400

    Server insert neume
    Finish server insert neume.
    
    Fixes #18

--- neonsrv/api.py
+++ neonsrv/api.py
@@ -74,69 +74,6 @@ class DeleteNeumeHandler(tornado.web.RequestHandler):
 
 class ChangeNeumePitchHandler(tornado.web.RequestHandler):
 
-    pitches = ["a", "b", "c", "d", "e", "f", "g"]
-
-    def find_difference(self, pname, octave, newpname, newoct):
-        """ Find the distance between two pictch/octave pairs.
-        Opposite of new_note. """
-
-        pname = pname.lower()
-        newpname = newpname.lower()
-        if isinstance(octave, basestring):
-            octave = int(octave)
-        if isinstance(newoct, basestring):
-            newoct = int(newoct)
-        oldidx = self.pitches.index(pname)
-        newidx = self.pitches.index(newpname)
-
-        difference = (newidx - oldidx) + (7 * (newoct - octave))
-        return difference
-
-    def new_note(self, pname, octave, difference):
-        """ If you have a pitch and octave and a direction
-        to move it in, give a new pitch and octave -
-        opposite of find_difference.
-
-        Returns a tuple (pname, oct) """
-
-        if isinstance(octave, basestring):
-            octave = int(octave)
-        pname = pname.lower()
-
-        oldidx = self.pitches.index(pname)
-
-        newidx = (oldidx + difference) % 7
-        octavech = (oldidx + difference) / 7
-        octave += octavech
-        return (self.pitches[newidx], "{}".format(octave))
-
-    def move_neume(self, neume, newp, newo):
-        """ Change the pitch of a neume """
-        if neume:
-            notes = neume.getDescendantsByName("note")
-            if len(notes):
-                first = notes[0]
-                o = first.getAttribute("oct").value
-                p = first.getAttribute("pname").value
-                difference = self.find_difference(p, o, newp, newo)
-                for n in notes:
-                    noteoct = n.getAttribute("oct").value
-                    notep = n.getAttribute("pname").value
-                    (changep, changeo) = self.new_note(notep, noteoct, difference)
-                    n.addAttribute("oct", changeo)
-                    n.addAttribute("pname", changep)
-
-    def update_or_add_zone(self, neume, ulx, uly, lrx, lry):
-        if neume:
-            facsid = neume.getAttribute("facs").value
-            if facsid:
-                zone = self.mei.getElementById(facsid)
-                if zone:
-                    zone.addAttribute("ulx", ulx)
-                    zone.addAttribute("uly", uly)
-                    zone.addAttribute("lrx", lrx)
-                    zone.addAttribute("lry", lry)
-
     def post(self, file):
         """ Change the pitch of a <neume> element.
         Pass in an argument called 'id' which is the id of a <neume>
@@ -149,63 +86,57 @@ class ChangeNeumePitchHandler(tornado.web.RequestHandler):
         no <zone> element then nothing will be added.
         """
 
-        neumeid = self.get_argument("id", "")
-        pname = self.get_argument("pname", "")
-        octave = self.get_argument("octave", "")
+        nid = str(self.get_argument("id", ""))
+        pname = str(self.get_argument("pname", ""))
+        octave = str(self.get_argument("oct", ""))
 
         mei_directory = os.path.abspath(conf.MEI_DIRECTORY)
         fname = os.path.join(mei_directory, file)
-        self.mei = XmlImport.documentFromFile(fname)
+        self.mei = XmlImport.read(fname)
 
-        neume = self.mei.get_by_id(neumeid)
+        neume = self.mei.getElementById(nid)
 
         self.move_neume(neume, pname, octave)
 
         # Bounding box
-        lrx = self.get_argument("lrx", None)
-        lry = self.get_argument("lry", None)
-        ulx = self.get_argument("ulx", None)
-        uly = self.get_argument("uly", None)
+        lrx = str(self.get_argument("lrx", None))
+        lry = str(self.get_argument("lry", None))
+        ulx = str(self.get_argument("ulx", None))
+        uly = str(self.get_argument("uly", None))
 
         if lrx and lry and ulx and uly:
-            update_or_add_zone(neume, ulx, uly, lrx, lry)
+            self.update_or_add_zone(neume, ulx, uly, lrx, lry)
 
         XmlExport.write(self.mei, fname)
         self.set_status(200)
 
 class InsertNeumeHandler(tornado.web.RequestHandler):
 
-    def do_insert(self, neume, zone, parentId, before=None):
-        """ Insert into the mei contents """
-        if zone:
-            neume.addAttribute("facs", zone.getId())
-
-        parent = self.mei.getElementById(parentId)
-        if parent and before:
-            before = self.mei.getElementById(before)
-            parent.addChildBefore(before, neume)
-        elif parent:
-            parent.addChild(neume)
-
-        surfaces = self.mei.getElementsByName("surface")
-        if len(surfaces) and zone:
-            surfaces[0].addChild(zone)
-            return surfaces[0].id
-        else:
-            return None
-
-    def get_new_neume(self, pname, octave):
-        """ Make a new note and return MEI object """
-        neume = MeiElement("neume")
-        neume.addAttribute("name", "punctum")
+    def create_new_punctum(self, pname, oct):
+        '''
+        Make a new punctum with the given pitch name and
+        octave and return the MEI element
+        '''
+        punctum = MeiElement("neume")
+        punctum.addAttribute("name", "punctum")
         nc = MeiElement("nc")
         note = MeiElement("note")
         note.addAttribute("pname", pname)
-        note.addAttribute("oct", octave)
-        neume.addChild(nc)
+        note.addAttribute("oct", oct)
+        punctum.addChild(nc)
         nc.addChild(note)
 
-        return neume
+        return punctum
+
+    def insert_punctum(self, punctum, beforeid):
+        before = self.mei.getElementById(beforeid)
+
+        # get layer element
+        parent = before.getParent()
+
+        if parent and before:
+            parent.addChildBefore(before, punctum)
+            
 
     def get_new_zone(self, ulx, uly, lrx, lry):
         zone = MeiElement("zone")
@@ -216,58 +147,53 @@ class InsertNeumeHandler(tornado.web.RequestHandler):
 
         return zone
 
-    def post(self, file):
-        """ Insert a new neume with a single note.
-        Returns the xml for the note, and the zone information.
-        Set the neume as a child of `parent'. If `before' is set, it is
-        expected to be a child of the parent and the new neume is 
-        inserted before the element with the id specified by before.
-        If `before' is not set, the new neume is added to the end of
-        `parent's children list.
-        Pass in ulx,uly,lrx,lry for bounding boxes if wanted.
+    def add_zone(self, neume, zone):
+        # add zone to the surfaces
+        surfaces = self.mei.getElementsByName("surface")
+        if len(surfaces) and zone:
+            surfaces[0].addChild(zone)
 
-        A json dictionary is returned:
-          {"neume": <neume>...,
-           "zone": <zone>...,
-           "zoneparent": m-nnnn-..."
-          }
-        where neume and zone are xml of the elements, and zoneparent
-        is the id of the element that the zone should be added
-        to the end of.
-        """
-        before = self.get_argument("before", None)
-        parent = self.get_argument("parent", None)
-        pname = self.get_argument("pname", "")
-        octave = self.get_argument("octave", "")
+        if zone:
+            # add facs id to neume
+            neume.addAttribute("facs", zone.getId())
+
+    def post(self, file):
+        '''
+        Insert a punctum before the given element. There is one case where
+        there is no element to insert before, when there is no subsequent staff.
+        In this case, the element is inserted at the end of the last system.
+        Also sets the bounding box information of the new punctum.
+        '''
+
+        beforeid = str(self.get_argument("beforeid", None))
+        pname = str(self.get_argument("pname", ""))
+        oct = str(self.get_argument("oct", ""))
+
+        # Bounding box
+        lrx = str(self.get_argument("lrx", None))
+        lry = str(self.get_argument("lry", None))
+        ulx = str(self.get_argument("ulx", None))
+        uly = str(self.get_argument("uly", None))
 
         mei_directory = os.path.abspath(conf.MEI_DIRECTORY)
         fname = os.path.join(mei_directory, file)
-        self.mei = XmlImport.documentFromFile(fname)
+        self.mei = XmlImport.read(fname)
 
-        neume = get_new_neume(pname, octave)
-        neume_xml = XmlExport.meiElementToText(neume)
-
-        # Bounding box
-        lrx = self.get_argument("lrx", None)
-        lry = self.get_argument("lry", None)
-        ulx = self.get_argument("ulx", None)
-        uly = self.get_argument("uly", None)
-
-        if lrx and lry and ulx and uly:
-            zone = self.get_new_zone(ulx, uly, lrx, lry)
-            zone_xml = XmlExport.meiElementToText(zone)
+        punctum = self.create_new_punctum(pname, oct)
+        zone = self.get_new_zone(ulx, uly, lrx, lry)
+        self.add_zone(punctum, zone)
+        
+        if beforeid is None:
+            # get last layer
+            layers = self.mei.getElementsByName("layer")
+            if len(layers):
+                layers[-1].addChild(punctum)
         else:
-            zone = None
-            zone_xml = ""
-
-        surface_id = self.do_insert(neume, zone, parent, before)
+            self.insert_punctum(punctum, beforeid)
 
         XmlExport.write(self.mei, fname)
 
-        result = {"zone": zone_xml,
-                "neume": neume_xml,
-                "zoneparent": surface_id
-                }
+        result = {"nid": punctum.getId()}
         self.write(json.dumps(result))
         self.set_status(200)
 
