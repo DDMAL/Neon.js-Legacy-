5fed113d842435428a810c79cecf275378e5de2e
Gregory Burlet <gregory.burlet@mail.mcgill.ca>
2012-06-14 15:52:06 -0400

    Custos ledger Lines
    Fixes #47

--- src/custosview.js
+++ src/custosview.js
@@ -30,9 +30,19 @@ Toe.View.CustosView = function(renderEngine) {
     this.rendEng = renderEngine;
 
     this.drawing = null;
+    this.ledgerLines = null;
 }
 
 Toe.View.CustosView.prototype.constructor = Toe.View.CustosView;
+
+Toe.View.CustosView.prototype.drawLedgerLines = function(staffPos, centre, width, staff) {
+    var cv = this;
+    if ((staffPos > 0 || staffPos < staff.props.numLines) && staffPos % 2 == 0) {
+        var y = staff.zone.uly - (staffPos*staff.delta_y/2);
+        var ledger = cv.rendEng.createLine([centre-width, y, centre+width, y]);
+        this.ledgerLines = this.rendEng.draw({static: [ledger], modify: []}, {group: true, selectable: false})[0];
+    }
+}
 
 /**
  * Renders the custos on the staffs
@@ -52,8 +62,10 @@ Toe.View.CustosView.prototype.renderCustos = function(custos) {
 
     // calculate the y position of the custos
     var custos_y = staff.zone.uly - custos.rootStaffPos*staff.delta_y/2;
-    var custosDwg = glyphCustos.clone().set({left: custos.zone.ulx + glyphCustos.centre[0], top: custos_y - glyphCustos.centre[1]/2});
+    var nc_x = custos.zone.ulx + glyphCustos.centre[0];
+    var custosDwg = glyphCustos.clone().set({left: nc_x, top: custos_y - glyphCustos.centre[1]/2});
 
+    this.drawLedgerLines(custos.rootStaffPos, nc_x, 4*glyphCustos.centre[0], staff);
 	this.drawing = this.rendEng.draw({static: [], modify: [custosDwg]}, {selectable: custos.props.interact, eleRef: custos})[0];
 }
 
@@ -61,11 +73,17 @@ Toe.View.CustosView.prototype.updateStaffPosition = function(custos) {
     if (!this.drawing) {
         throw new Error("Custos: update method called, but there exists no drawing to update.");
     }
+    if (this.ledgerLines) {
+        this.rendEng.canvas.remove(this.ledgerLines);
+        this.ledgerLines = null;
+    }
 
     var staff = custos.staff;
 
     var glyphTop = staff.zone.uly - custos.rootStaffPos*staff.delta_y/2 - this.drawing.currentHeight/4;
     this.drawing.set({top: glyphTop});
+
+    this.drawLedgerLines(custos.rootStaffPos, this.drawing.left, (3/2)*this.drawing.currentWidth, staff);
 
     this.rendEng.repaint();
 
