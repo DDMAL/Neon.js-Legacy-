e07074761f7b94b2d618690de5c2b9029fa64c03
Gregory Burlet <gregory.burlet@mail.mcgill.ca>
2012-07-09 11:35:34 -0400

    Add trie code to compilation
    To package everything up nicely.

--- build/build.sh
+++ build/build.sh
@@ -10,5 +10,5 @@ then
 fi
 
 echo "Compiling Neon.js with google closure ..."
-java -jar compiler.jar --js ../src/toe.js ../src/clef*.js ../src/custos*.js ../src/division*.js ../src/glyph.js ../src/gui.js ../src/nc.js ../src/neon.js ../src/neume*.js ../src/ornament.js ../src/page*.js ../src/renderengine.js ../src/staff*.js --js_output_file neon.min.js
+java -jar compiler.jar --js ../src/toe.js ../src/clef*.js ../src/custos*.js ../src/division*.js ../src/glyph.js ../src/gui.js ../src/nc.js ../src/neon.js ../static/js/support/searchtree.min.js ../src/neume*.js ../src/ornament.js ../src/page*.js ../src/renderengine.js ../src/staff*.js --js_output_file neon.min.js
 echo "Built to neon.min.js"
